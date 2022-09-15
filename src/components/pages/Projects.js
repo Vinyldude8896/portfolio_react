dcd3a2317eb42f79ef70be20a04958a74b450eda
Kevin Reid <kevinnivek@me.com>
2022-09-15 12:29:23 -0400

    deleted Projects folder

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -8,7 +8,7 @@ const Projects = () => {
       className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-14 lg:px-8 xl:mt-14"
     >
       <div className="sm:text-center lg:text-left">
-        <h1 classNames="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-5xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-5xl">
           <span className="block xl:inline pr-8">Welcome to my </span>
           <span className="block text-indigo-600 xl:inline">
             {" "}
