8568c32826af41741218309f1b969b2b77fbc1d8
Kevin Reid <kevinnivek@me.com>
2022-09-17 23:39:44 -0400

    Updates

---
+++ precache-manifest.70fd5f8d430c8ad0f6ce8f5ff1ae3ba8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faa7d0a4de1a7c713562",
+    "url": "/portfolio-react/static/css/main.9b79347e.chunk.css"
+  },
+  {
+    "revision": "faa7d0a4de1a7c713562",
+    "url": "/portfolio-react/static/js/main.faa7d0a4.chunk.js"
+  },
+  {
+    "revision": "40029165541dfd61b451",
+    "url": "/portfolio-react/static/js/1.40029165.chunk.js"
+  },
+  {
+    "revision": "9338e8a022e42c6bc02f",
+    "url": "/portfolio-react/static/js/runtime~main.9338e8a0.js"
+  },
+  {
+    "revision": "c045002b87bd0c9e6f1c5b5da1a89d41",
+    "url": "/portfolio-react/static/media/Kevin_Reid_Resume_Development.c045002b.pdf"
+  },
+  {
+    "revision": "a3f26c7a5cfdfd44ab4d94203edd4a35",
+    "url": "/portfolio-react/static/media/Kevin_4.a3f26c7a.JPG"
+  },
+  {
+    "revision": "c73a4b9e39fbff7278080541b3f3c800",
+    "url": "/portfolio-react/static/media/tech_blog.c73a4b9e.png"
+  },
+  {
+    "revision": "945751699cf4857af77ed147a1ef9820",
+    "url": "/portfolio-react/static/media/IBetter.94575169.png"
+  },
+  {
+    "revision": "738caa3e6c14d312f7a06ef43e1b87a6",
+    "url": "/portfolio-react/static/media/Artist_music_tours_1.738caa3e.jpeg"
+  },
+  {
+    "revision": "d4ba5f3b428fe8a69dad0e338afd77e7",
+    "url": "/portfolio-react/static/media/Weather_dashboard.d4ba5f3b.jpeg"
+  },
+  {
+    "revision": "26e8140627800eb1cfeddcf7766ba472",
+    "url": "/portfolio-react/static/media/Employee_Tracker.26e81406.png"
+  },
+  {
+    "revision": "a82e24e574d2719caecfaa93a4686bae",
+    "url": "/portfolio-react/static/media/Coding_quiz.a82e24e5.png"
+  },
+  {
+    "revision": "d2bc28c6d7e2bfb2c361d2e7c8566d99",
+    "url": "/portfolio-react/static/media/css3.d2bc28c6.png"
+  },
+  {
+    "revision": "83cc8a33814741b8fc665b34a1d2515d",
+    "url": "/portfolio-react/static/media/jquery.83cc8a33.png"
+  },
+  {
+    "revision": "1962f6271f8b72797685e2eb22597511",
+    "url": "/portfolio-react/static/media/node-js.1962f627.png"
+  },
+  {
+    "revision": "3ed2736508268f29b15be2b7dd750fcb",
+    "url": "/portfolio-react/static/media/react.3ed27365.png"
+  },
+  {
+    "revision": "9dd8e75678aced1e759b58abd886d81d",
+    "url": "/portfolio-react/static/media/mysql.9dd8e756.png"
+  },
+  {
+    "revision": "33b50b474848a1dedce2254d647f139a",
+    "url": "/portfolio-react/static/media/sequelize.33b50b47.png"
+  },
+  {
+    "revision": "f5171dccbb5462e7472cd16100e718de",
+    "url": "/portfolio-react/static/media/mongodb.f5171dcc.png"
+  },
+  {
+    "revision": "a6432a9ade40553054ce90804159348f",
+    "url": "/portfolio-react/static/media/Running.a6432a9a.gif"
+  },
+  {
+    "revision": "a9ee58dfa5a00c2f2679033095a851e8",
+    "url": "/portfolio-react/static/media/Kevin.a9ee58df.JPG"
+  },
+  {
+    "revision": "1464c15faad737cbac5731f9669a2fa3",
+    "url": "/portfolio-react/index.html"
+  }
+];
