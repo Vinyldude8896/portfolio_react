b4210d865b5fe318867d846bc6ee90d3a5c6cbcc
Kevin Reid <kevinnivek@me.com>
2022-10-28 11:48:42 -0400

    added section to resume

--- src/components/pages/Resume_PDF.js
+++ src/components/pages/Resume_PDF.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import kevinResume from '../../Assets/Kevin_Reid_Resume_Development.pdf'
+import React from "react";
+import kevinResume from "../../Assets/Kevin_Reid_Resume_Development.pdf";
 
 const Resume_PDF = () => {
-    return (<>
-        <object className="resumePDF" width="100%" height="1400" data={kevinResume} type="application/pdf" aria-label="resume" scrollbar="1|0" view="Fit" />
-    </>)
-}
+  return (
+    <section
+      id="Skills"
+      className="mx-auto mt-10 max-w-12xl px-4 sm:mt-12 sm:px-6 md:mt-12 lg:mt-14 lg:px-8 xl:mt-14"
+    >
+      <object
+        className="resumePDF"
+        width="100%"
+        height="1400"
+        data={kevinResume}
+        type="application/pdf"
+        aria-label="resume"
+        scrollbar="1|0"
+        view="Fit"
+      />
+    </section>
+  );
+};
 
 export default Resume_PDF;
