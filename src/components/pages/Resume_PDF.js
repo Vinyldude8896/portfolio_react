99274befcc4c6278c80caeb07125b5da04790694
Kevin Reid <kevinnivek@me.com>
2022-10-28 11:34:50 -0400

    added media styling

--- src/components/pages/Resume_PDF.js
+++ src/components/pages/Resume_PDF.js
@@ -3,7 +3,7 @@ import kevinResume from '../../Assets/Kevin_Reid_Resume_Development.pdf'
 
 const Resume_PDF = () => {
     return (<>
-        <object width="100%" height="1800" data={kevinResume} type="application/pdf" aria-label="resume" scrollbar="1|0" view="Fit" />
+        <object className="resumePDF" width="100%" height="1400" data={kevinResume} type="application/pdf" aria-label="resume" scrollbar="1|0" view="Fit" />
     </>)
 }
 
