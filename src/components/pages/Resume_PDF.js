f8426c7f04b3f1a6ae6fbae1bdbcdced0198180c
Kevin Reid <kevinnivek@me.com>
2022-10-28 11:59:04 -0400

    changed styling resume section

--- src/components/pages/Resume_PDF.js
+++ src/components/pages/Resume_PDF.js
@@ -4,18 +4,18 @@ import kevinResume from "../../Assets/Kevin_Reid_Resume_Development.pdf";
 const Resume_PDF = () => {
   return (
     <section
-      id="Skills"
+      id="Resume"
       className="mx-auto mt-10 max-w-12xl px-4 sm:mt-12 sm:px-6 md:mt-12 lg:mt-14 lg:px-8 xl:mt-14"
     >
       <object
         className="resumePDF"
         width="100%"
-        height="1400"
+        height="1000"
         data={kevinResume}
         type="application/pdf"
         aria-label="resume"
-        scrollbar="1|0"
-        view="Fit"
+        // scrollbar="1|0"
+        // view="Fit"
       />
     </section>
   );
